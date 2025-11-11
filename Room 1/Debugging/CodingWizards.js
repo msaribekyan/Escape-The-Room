39307a4038074ed49817e74a948aa28d7a0a87b5
Jora <91777313+Jora001@users.noreply.github.com>
2025-11-11 18:14:49 +0400

    added codding_room1.js file

--- Room 1/Debugging/CodingWizards.js
+++ Room 1/Debugging/CodingWizards.js
@@ -1,7 +1,7 @@
 //Fix the scope issue so that the outer x remains 1 and the inner x is only 2.
 
 function testScope() {
-  var x = 1;
+  let x = 1;
   if (true) {
     let x = 2;
     console.log("Inside block:", x);
