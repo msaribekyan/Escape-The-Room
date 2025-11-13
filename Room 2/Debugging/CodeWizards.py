c3da15e5c5cc20ffc0b8c777e7ce14761942567b
sona-avagyan <sona.avagyan@student.rau.am>
2025-11-13 15:28:22 +0400

    CodeWizards.py
    Python lists don't have .reversed() method, it's a built-in function reversed(), that returns an iterator. So we also need to use list() to get list.

--- Room 2/Debugging/CodeWizards.py
+++ Room 2/Debugging/CodeWizards.py
@@ -1,5 +1,5 @@
 #Fix this code so it returns a reversed copy of the list without modifying the original one.
 def reverse_list(lst):
-    return lst.reversed()
+    return list(reversed(lst))
 
 print(reverse_list([1, 2, 3])) #Expected Output: "3, 2, 1"
