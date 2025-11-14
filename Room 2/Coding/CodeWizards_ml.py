0a95f62a6fe1c2ea72453eaa5ad035ceb6d35404
Mher Saribekyan <saribekyanmher@gmail.com>
2025-11-14 09:17:11 +0400

    room 2 ml coding error handling

--- Room 2/Coding/CodeWizards_ml.py
+++ Room 2/Coding/CodeWizards_ml.py
@@ -5,7 +5,7 @@ from numpy.linalg import norm
 
 def cosine_similarity(vec1: np.ndarray, vec2: np.ndarray):
     
-    if vec1.ndim != 1 or vec1.shape != vec2.shape:
-       return 0
-
-    return np.dot(vec1, vec2) / (norm(vec1) * norm(vec2))
+    try:
+        return np.dot(vec1, vec2) / (norm(vec1) * norm(vec2))
+    except:
+        return 0
