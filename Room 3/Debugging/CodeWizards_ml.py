a5479fd3ffa33d57fc008c36fdb3a3126cea7d3b
Mher Saribekyan <saribekyanmher@gmail.com>
2025-11-15 18:25:12 +0400

    done debugging problem

--- Room 3/Debugging/CodeWizards_ml.py
+++ Room 3/Debugging/CodeWizards_ml.py
@@ -9,5 +9,5 @@ X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 model = SVC(kernel='linear', probability=True)
-model.fit(X)
+model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
