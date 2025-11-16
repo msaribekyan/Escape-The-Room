8fadf4dc40c76f27737fd994ca515496401ca479
sona-avagyan <sona.avagyan@student.rau.am>
2025-11-16 23:51:54 +0400

    CodeWizards.py
    x doesn't have method key. lst consists of dicts. dicts use indexing

--- Room 3/Debugging/CodeWizards.py
+++ Room 3/Debugging/CodeWizards.py
@@ -1,6 +1,6 @@
 #Fix the sorting function to correctly access dictionary keys without causing any error.
 
 def sort_custom(lst, key):
-    return sorted(lst, key=lambda x: x.key)
+    return sorted(lst, key=lambda x: x[key])
 
 print(sort_custom([{'a': 3}, {'a': 1}, {'a': 2}], 'a'))
