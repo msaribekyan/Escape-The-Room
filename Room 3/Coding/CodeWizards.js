3ffc2b1cae1878648f484900022a28e79bca428e
Jora Hovsepyan <91777313+Jora001@users.noreply.github.com>
2025-11-16 11:13:47 +0400

    Update CodeWizards.js

--- Room 3/Coding/CodeWizards.js
+++ Room 3/Coding/CodeWizards.js
@@ -1,3 +1,31 @@
 // Write a function that takes an object that may contain nested objects and returns a new object where the nesting is flattened. Each key should represent its path using a dot (.) to separate levels.
 
 // Note: You may need to use recursion.
+
+
+
+function flattenObject(obj, parentKey = '', result = {}) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const newKey = parentKey ? `${parentKey}.${key}` : key;
+            if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
+                flattenObject(obj[key], newKey, result);
+            } else {
+                result[newKey] = obj[key];
+            }
+        }
+    }
+    return result;
+}
+
+const profile = {
+    user: {
+        name: {
+            first: "Jane",
+            last: "Doe"
+        },
+        age: 28
+    }
+};
+
+console.log(flattenObject(profile));
