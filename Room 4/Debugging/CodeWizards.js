5bd5bea65baf413ee4772bf2a5978dd37bbebd69
Jora Hovsepyan <91777313+Jora001@users.noreply.github.com>
2025-11-19 07:30:00 +0400

    Fix filter function to correctly filter positives

--- Room 4/Debugging/CodeWizards.js
+++ Room 4/Debugging/CodeWizards.js
@@ -1,8 +1,7 @@
 //Fix the code to correctly filter positive numbers.
   
-  function filterPositives(arr) {
-    return arr.map(num => num > 0);
-  }
-  
-  console.log(filterPositives([-1, 2, -3, 4]));
-  
+ function filterPositives(arr) {
+  return arr.filter(num => num > 0);
+}
+
+console.log(filterPositives([-1, 2, -3, 4]));
