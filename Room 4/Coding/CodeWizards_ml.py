288b8f9de9e280a55e87a7c6522f9be5056e7acf
Mher Saribekyan <saribekyanmher@gmail.com>
2025-11-19 09:04:16 +0400

    solved ml problems

--- Room 4/Coding/CodeWizards_ml.py
+++ Room 4/Coding/CodeWizards_ml.py
@@ -1,1 +1,11 @@
 # Write a function that calculates the Euclidean distance between two NumPy vectors.
+
+import numpy as np
+from numpy.linalg import norm
+
+def euclidean_distance(vec1: np.ndarray, vec2: np.ndarray):
+
+    try:
+        return norm(vec2 - vec1)
+    except:
+        return 0
