d013a56572baa22bf57856ed1cb2bee76427b9b7
Mher Saribekyan <saribekyanmher@gmail.com>
2025-11-22 10:35:53 +0400

    finished ml coding

--- Room 5/Coding/CodeWizards_ml.py
+++ Room 5/Coding/CodeWizards_ml.py
@@ -1,6 +1,18 @@
 # Write a function that splits a dataset into train, validation, and test sets manually using slicing (cannot use sklearn).
-import numpy as np
+import pandas as pd
 
-def data_split(X, y, train=0.8, val=0.1, test=0.1):
-    data = np.column_stack((X, y))
-    return (X_train, y_train, X_validation, y_validation, X_test, Y_test)
+def data_split(data, train=0.8, val=0.1, test=0.1):
+    if(train+val+test != 1):
+        return (data)
+
+    data = data.sample(frac=1)
+
+    total_rows = data.shape[0]
+    train_size = int(total_rows*train)
+    validation_size = int(total_rows*val)
+
+    data_train = data[0:train_size]
+    data_validation = data[train_size:(train_size+validation_size)]
+    data_test = data[(train_size+validation_size):]
+    
+    return (data_train, data_validation, data_test)
