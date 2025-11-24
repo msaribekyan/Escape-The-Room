d28545f4ee39e5ffff5645133f14494cc871522b
Jora Hovsepyan <91777313+Jora001@users.noreply.github.com>
2025-11-24 11:02:48 +0400

    Update CodeWizards.js

--- Room 6/Debugging/CodeWizards.js
+++ Room 6/Debugging/CodeWizards.js
@@ -1,6 +1,12 @@
 //Fix the async function to properly wait for multiple Promises using the correct syntax.
 
 async function loadData() {
-    const result = await Promise.all("first", "second");
+    const result = await Promise.all([
+        Promise.resolve("first"),
+        Promise.resolve("second")
+    ]);
     console.log(result);
-  }
+}
+
+
+loadData();
