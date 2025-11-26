dfdd6d754069f8cfe30cb314a9f0f9334aa0f07e
Jora Hovsepyan <91777313+Jora001@users.noreply.github.com>
2025-11-26 10:45:14 +0400

    Update CodeWizards.js

--- Room 7/Debugging/CodeWizards.js
+++ Room 7/Debugging/CodeWizards.js
@@ -1,11 +1,12 @@
 //Fix the async function to correctly handle the returned string.
 
 async function getData() {
-    return "data";
-  }
-  
-  async function useData() {
-    const response = getData();
-    console.log(await response.json());
-  }
-  
+  return "data";
+}
+
+async function useData() {
+  const response = await getData();
+  console.log(response);
+}
+
+useData();
