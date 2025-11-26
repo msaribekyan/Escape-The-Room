5a49f6b485d7dc5d48b8c12aa76ccdf9a53293d5
yabisoghomonyan <yabisoghomonyan@gmail.com>
2025-11-26 22:07:29 +0400

    CodeWizards.py

--- Room 8/Debugging/CodeWizards.py
+++ Room 8/Debugging/CodeWizards.py
@@ -1,6 +1,6 @@
 #Fix the function to square only numeric values.
 
 def sum_squares(lst):
-    return sum(x**2 for x in lst if type(x) == int or float)
+    return sum(x**2 for x in lst if isinstance(x,(int,float)))
 
 print(sum_squares([1, 2, 'three', 4]))
