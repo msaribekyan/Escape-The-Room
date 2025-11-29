068122e9caa9583b4f339ba8ffd1929719889106
yabisoghomonyan <yabisoghomonyan@gmail.com>
2025-11-29 21:53:09 +0400

    CodeWizards.py

--- Room 9/Debugging/CodeWizards.py
+++ Room 9/Debugging/CodeWizards.py
@@ -1,11 +1,4 @@
 #Fix the function to properly reverse a dictionary and handle cases where multiple keys may share a value.
-
-def reverse_dict(d):
-    return {k: v for v, k in d.items()}
-
-print(reverse_dict({'a': 1, 'b': 2}))
-
-# my version 
 def reverse_dict(d):
     rev_dict={}
     for k, v in d.items():
