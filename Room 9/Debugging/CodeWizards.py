7dd568db6a9cf7ccbd68709b9b08f12a168860b1
yabisoghomonyan <yabisoghomonyan@gmail.com>
2025-11-29 19:30:01 +0400

    CodeWizards.py

--- Room 9/Debugging/CodeWizards.py
+++ Room 9/Debugging/CodeWizards.py
@@ -4,3 +4,16 @@ def reverse_dict(d):
     return {k: v for v, k in d.items()}
 
 print(reverse_dict({'a': 1, 'b': 2}))
+
+# my version 
+def reverse_dict(d):
+    rev_dict={}
+    for k, v in d.items():
+        if v not in rev_dict:
+            rev_dict[v]=k
+        else:
+            if not isinstance(rev_dict[v],list):
+                rev_dict[v]=[rev_dict[v]] 
+            rev_dict[v].append(k)
+    return rev_dict
+print(reverse_dict({'a': 1, 'b': 2}))#output is {1: 'a', 2: 'b'}
