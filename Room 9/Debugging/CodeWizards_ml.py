befec609de4e6ed978e994ce890fad04a7486e0e
Mher Saribekyan <saribekyanmher@gmail.com>
2025-11-30 02:45:51 +0400

    fixed room 9 ml code

--- Room 9/Debugging/CodeWizards_ml.py
+++ Room 9/Debugging/CodeWizards_ml.py
@@ -1,10 +1,14 @@
 #Fix the order of operations and ensure the model is trained using proper data structures before prediction.
 
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 model = LogisticRegression()
-X = [[0.1, 0.2], [0.3, 0.4]]
-y = [0, 1]
+X = np.array([
+    [0.1, 0.2],
+    [0.3, 0.4]
+])
+y = np.array([0, 1])
 
-model.predict(X)
 model.fit(X, y)
+model.predict(X)
