a2c410aba06b52733b4f87177b461befa5b47fc5
sona-avagyan <sona.avagyan@student.rau.am>
2025-11-29 21:08:56 +0400

    CodeWizards.py

--- Room 9/Coding/CodeWizards.py
+++ Room 9/Coding/CodeWizards.py
@@ -1,3 +1,14 @@
-# Write a function that receives a list of tuples and returns a dictionary grouping by the first item of each tuple.
+#For Python: Write a function that receives a list of tuples and returns a dictionary grouping by the first item of each tuple.
+#Example: [('a', 1), ('b', 2), ('a', 3)] → {'a': [1, 3], 'b': [2]}
 
-# Example: [('a', 1), ('b', 2), ('a', 3)] → {'a': [1, 3], 'b': [2]}
+def make_dict(lst):
+    res_dict = {}
+    for el in lst:
+        key = el[0]
+        values = el[1:]
+        if key not in res_dict:
+            res_dict[key] = []
+        res_dict[key].extend(values)
+    return res_dict
+
+print(make_dict([('a', 1, 5), ('b', 2), ('a', 3)])) #{'a': [1, 5, 3], 'b': [2]} 
