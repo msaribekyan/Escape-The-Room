deb1e8f81a99ccbc08e0e90bd9d11528a8614bf0
Jora Hovsepyan <91777313+Jora001@users.noreply.github.com>
2025-11-30 10:39:04 +0400

    Update CodeWizards.js

--- Room 9/Debugging/CodeWizards.js
+++ Room 9/Debugging/CodeWizards.js
@@ -1,10 +1,8 @@
-//Fix the function to correctly parse and log the fetched JSON data from the given API.
-
 function fetchData(url) {
-    fetch(url)
-      .then(response => response.json)
-      .then(data => console.log(data))
-      .catch(error => console.error("Error:", error));
-  }
-  
-  fetchData("https://jsonplaceholder.typicode.com/posts/1");
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
+}
+
+fetchData("https://jsonplaceholder.typicode.com/posts/1");
