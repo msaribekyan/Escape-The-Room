c5929f68f46777a58562dad5ff85e7b6c79ea62a
Jora Hovsepyan <91777313+Jora001@users.noreply.github.com>
2025-12-02 11:36:57 +0400

    Fix string validation condition in validateString

--- Room 10/Debugging/CodeWizards.js
+++ Room 10/Debugging/CodeWizards.js
@@ -1,11 +1,11 @@
 //Fix the condition to properly check if the string is 5 characters long and contains "js".
 
 function validateString(str) {
-    if (str.length = 5 && str.includes("js")) {
-      return "Good job";
-    } else {
-      return "Try again";
-    }
+  if (str.length === 5 && str.includes("js")) {
+    return "Good job";
+  } else {
+    return "Try again";
   }
-  
-  console.log(validateString("js123"));
+}
+
+console.log(validateString("js123"));
